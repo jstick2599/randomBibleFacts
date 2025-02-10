ed5c03ce89b7ddaa20629b0cc1c8ab4dbd43bc27
Joshua <jstick2599@gmail.com>
2025-02-10 12:35:23 -0700

    added about us pic of the two creators

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,8 @@
 
 import React from "react";
 import "../styling/AboutUs.css";
+import aboutUsPic from "../images/aboutUsPic.jpg";
+import cactus from "../images/cactus.png";
 
 function AboutUs(){
     return (
@@ -10,6 +12,10 @@ function AboutUs(){
                 <source src="/aboutUsVideo.mov" type="video/mp4" />Your browser does not support the video tag.</video>
             </div>
             <div className="paragraphs">
+                <div className="images">
+                    <img src={aboutUsPic} alt="Two creators of website" id="aboutUsImg"/>
+                    {/* <img src={cactus} alt="Cactus" id="cactusImg"/> */}
+                </div>
                 <p className="bio">Welcome to VerseFacts.com, a Christian website dedicated to sharing the truth of God's Word with the world. Our mission is twofold: to help non-believers encounter the life-changing power of Jesus Christ and to equip believers to grow deeper in their faith.</p>
                 <p className="bio">We live in a time when misinformation and doubt can cloud the truth of the Gospel. At VerseFacts.com, we aim to cut through the noise by providing biblical insights, apologetics, and faith-based resources that strengthen your understanding of Scripture. Whether you are exploring Christianity for the first time or seeking to deepen your walk with Christ, our content is designed to guide, encourage, and inspire.</p>
                 <p className="bio">We believe the Bible is the inerrant Word of God, Jesus is the only way to salvation, and faith in Him transforms lives. Through carefully researched articles, compelling evidence for biblical truth, and practical teachings, we strive to share the Good News with clarity and conviction.</p>
