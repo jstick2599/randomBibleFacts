10e176f8984c93fe3f1cb4ad643e772bce4ee3a7
Joshua <jstick2599@gmail.com>
2025-02-11 12:35:08 -0700

    added more styling to about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -8,6 +8,9 @@ import ReactPlayer from "react-player";
 function AboutUs(){
     return (
         <div className="aboutUsPage">
+            <div className="aboutUsPageTitle">
+                <h1>About Us</h1>
+            </div>
             <div className="video">
                 {/* <video width="600" height="100vh" controls> */}
                 {/* <source src="/aboutUsVideo.mov" type="video/mp4" />Your browser does not support the video tag.</video> */}
@@ -20,7 +23,7 @@ function AboutUs(){
                 />
 
             </div>
-            <div className="paragraphs">
+            {/* <div className="paragraphs"> */}
                 <div className="images">
                     <img src={aboutUsPic} alt="Two creators of website" id="aboutUsImg"/>
                     {/* <img src={cactus} alt="Cactus" id="cactusImg"/> */}
@@ -29,7 +32,7 @@ function AboutUs(){
                 <p className="bio">We live in a time when misinformation and doubt can cloud the truth of the Gospel. At VerseFacts.com, we aim to cut through the noise by providing biblical insights, apologetics, and faith-based resources that strengthen your understanding of Scripture. Whether you are exploring Christianity for the first time or seeking to deepen your walk with Christ, our content is designed to guide, encourage, and inspire.</p>
                 <p className="bio">We believe the Bible is the inerrant Word of God, Jesus is the only way to salvation, and faith in Him transforms lives. Through carefully researched articles, compelling evidence for biblical truth, and practical teachings, we strive to share the Good News with clarity and conviction.</p>
                 <p className="bio">Thank you for visiting VerseFacts.com—where truth meets faith, and faith leads to life.</p>
-            </div>
+            {/* </div> */}
                 
 
         </div>
